68ab6c2f161ee7bfde21d366469bc6af4e9f9b34
Mateusz Samsel <msamsel@users.noreply.github.com>
2019-11-02 20:57:46 +0100

    Add simple board componenets.

--- src/components/game/Board.vue
+++ src/components/game/Board.vue
@@ -1,18 +1,28 @@
 <template>
 	<div class="minesweeper-board" :style="cssVars" >
-
+		<BoardTile v-for="i in numberOfTiles" :key="i" />
 	</div>
 </template>
 
 <script>
+import BoardTile from '@/components/game/BoardTile.vue';
+
 export default {
 	name: 'Board',
-	props: [ 'size' ],
+	props: [ 'size', 'board' ],
+	components: {
+		BoardTile
+	},
 	computed: {
+		numberOfTiles() {
+			return this.size.width * this.size.height;
+		},
 		cssVars() {
 			return {
-				'--board-width': this.size.width,
-				'--board-height': this.size.height
+				'--board-width': `${ this.size.width * 20 }px`,
+				'--board-height': `${ this.size.height * 20 }px`,
+				'--board-columns': this.size.width,
+				'--board-rows': this.size.height
 			};
 		}
 	}
@@ -21,8 +31,11 @@ export default {
 
 <style scoped>
 	.minesweeper-board {
+		margin: 0 auto;
 		display: grid;
-		grid-template-columns: repeat( var( --board-width ), 1fr );
-		grid-template-rows: repeat( var( --board-height ), 1fr )
+		width: var( --board-width );
+		height: var( --board-height );
+		grid-template-columns: repeat( var( --board-columns ), 1fr );
+		grid-template-rows: repeat( var( --board-rows ), 1fr )
 	}
 </style>
---
+++ src/components/game/BoardTile.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="minesweeper-board__tile">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BoardTile',
+	props: [ 'gridPosition' ]
+};
+</script>
+
+<style scoped>
+	.minesweeper-board__tile {
+		width: 20px;
+		height: 20px;
+		border: 1px solid black;
+	}
+</style>
